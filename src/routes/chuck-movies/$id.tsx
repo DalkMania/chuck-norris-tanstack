63184137e167d4d0d399a9686c68c71d7581a55b
Niklas Dahlqvist <dalkmania@gmail.com>
2025-12-30 00:41:20 +0100

    Test ENV Vars

--- src/routes/chuck-movies/$id.tsx
+++ src/routes/chuck-movies/$id.tsx
@@ -1,10 +1,16 @@
 import { MovieDetails } from '@/pages/MovieDetails'
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 import { TMDB } from 'tmdb-ts'
+
+const getRuntimeVar = createServerFn({ method: 'GET' }).handler(() => {
+  return process.env.TMDB_API_KEY // notice `process.env` on the server, and no `VITE_` prefix
+})
 
 export const Route = createFileRoute('/chuck-movies/$id')({
   loader: async ({ params }) => {
-    const tmdb = new TMDB(process.env.TMDB_API_KEY!)
+    const foo = await getRuntimeVar()
+    const tmdb = new TMDB(foo!)
     const movie = await tmdb.movies.details(Number(params.id))
     return movie
   },
--- src/routes/chuck-movies/index.tsx
+++ src/routes/chuck-movies/index.tsx
@@ -1,10 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Movies } from '@/pages/Movies'
 import { PersonMovieCast, TMDB } from 'tmdb-ts'
+import { createServerFn } from '@tanstack/react-start'
+
+const getRuntimeVar = createServerFn({ method: 'GET' }).handler(() => {
+  return process.env.TMDB_API_KEY // notice `process.env` on the server, and no `VITE_` prefix
+})
 
 export const Route = createFileRoute('/chuck-movies/')({
   loader: async () => {
-    const tmdb = new TMDB(process.env.TMDB_API_KEY!)
+    const foo = await getRuntimeVar()
+    const tmdb = new TMDB(foo!)
     const movies = await tmdb.people.movieCredits(51576)
     return movies.cast.sort(
       (a, b) => parseFloat(b.release_date) - parseFloat(a.release_date),
